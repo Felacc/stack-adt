bba7ab7a4f7af9e9e04bf0448246018e2e753a1d
Felacc <98856469+Felacc@users.noreply.github.com>
2025-03-13 01:09:08 -0700

    Create Ics124Queue.java
    Made according to UML diagram supplied by prof.

---
+++ src/ics124/c0523788/assignment3/Ics124Queue.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ics124.c0523788.assignment3;
+
+/**
+ *
+ * @author felix
+ */
+public class Ics124Queue<E> {
+    
+    // Default Constructor
+    public Ics124Queue() {
+        
+    }
+    
+    // Getters and Setters
+    
+    
+    // Methods
+    public boolean add(E e)  {
+        throw new UnsupportedOperationException();
+    }
+    
+    public E element() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public boolean offer(E e){
+        throw new UnsupportedOperationException();
+    }
+    
+    public E peek() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public E poll() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public E remove() {
+        throw new UnsupportedOperationException();
+    }
+    
+    
+        
+}
